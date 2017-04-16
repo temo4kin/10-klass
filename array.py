951e8004686fa4e93fa5d8ef4f8b71bfd43833bc
Temo4kin <temo4kinn@gmail.com>
2017-04-16 20:40:14 +0300

    Работа с массивами-4

--- array.py
+++ array.py
@@ -7,9 +7,14 @@ b = ['Russia', 'United Kingdom', 'France']
 print(len(a))
 '''
 
+tsred = 0
 temp = []
 for i in range(12):
     a = float(input('Введите значение температуры: '))
     temp.insert(i, a)
 
+for o in temp:
+    tsred += o
+
 print(temp)
+print(tsred/12)
