cb36fdd4f4e676c09ed00d5fc8d5cb7b9d82524d
Temo4kin <temo4kinn@gmail.com>
2017-04-16 20:35:19 +0300

    Работа с массивами-3

--- array.py
+++ array.py
@@ -1,7 +1,15 @@
-# Средняя температура за несколько лет
-
+# Среднегодовая температура
+'''
 a = 'london', 'Moscow', 'Paris'
 
 b = ['Russia', 'United Kingdom', 'France']
 
 print(len(a))
+'''
+
+temp = []
+for i in range(12):
+    a = float(input('Введите значение температуры: '))
+    temp.insert(i, a)
+
+print(temp)
